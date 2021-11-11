7cfd196a5d0764d96c1c9f1ba25ff83c07d0cae4
ranran <ranran9g@gmail.com>
2021-11-11 23:34:27 +0900

    proxy.pacの文字起こし用簡易スクリプト

---
+++ parser.py
@@ -0,0 +1,20 @@
+# nginxのエラーログ　docker logs nginx > out.log
+test_data = open("out.log", "r")
+score = {}
+for line in test_data:
+    try:
+        host = line.split("CONNECT ")[1].split(":")[0]
+        domain = host.split(".")[-2] + "." + host.split(".")[-1] 
+        if domain in score:
+            score[domain] += 1
+        else:
+            score[domain] = 1
+    except:
+        None
+# ログのホスト名の上位をproxy.pacに転記するための文字起こし
+score_sorted = sorted(score.items(), key=lambda x:x[1],reverse=True)
+for item in score_sorted:
+    if item[1] > 50:
+        #print(item[0],item[1])
+        print('shExpMatch(host, "*.{0}") || '.format(item[0]))
+#print(score_sorted)
